52477d05cbe287c36c47a2a2813f4119f3dc8c39
Salers <salersaimlisse@gmail.com>
2022-02-05 10:41:43 +0100

    Update ThemeLoader.java

--- src/main/java/fr/furyzen/oneesan/util/theme/ThemeLoader.java
+++ src/main/java/fr/furyzen/oneesan/util/theme/ThemeLoader.java
@@ -2,36 +2,49 @@ package fr.furyzen.oneesan.util.theme;
 
 import fr.furyzen.oneesan.Oneesan;
 import fr.furyzen.oneesan.configuration.Configuration;
-import lombok.Getter;
 import lombok.SneakyThrows;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 //TODO: make a thememanager, i guess lol ?
 public enum ThemeLoader {
     INSTANCE;
 
-    private final File themeFile;
+    private File themeFile;
 
     private FileConfiguration themeConfiguration;
     private String theme;
 
 
-    @SneakyThrows
-    ThemeLoader() {
-        themeFile = new File(Oneesan.INSTANCE.getPlugin().getDataFolder(), "theme.yml");
-        if (!themeFile.exists())
-            if(themeFile.getParentFile().mkdirs())
-                Oneesan.INSTANCE.getPlugin().saveResource("theme.yml", false);
+    private void saveFile() {
+        this.themeFile = new File(Oneesan.INSTANCE.getPlugin().getDataFolder(), "theme.yml");
+        this.themeConfiguration = YamlConfiguration.loadConfiguration(themeFile);
+
+        try {
+            themeConfiguration.save(themeFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     @SneakyThrows
     public void load(Configuration configuration) {
+        saveFile();
+
+        if(themeFile.length() == 0) {
+            themeConfiguration.options().copyDefaults(true);
+        }
+
         theme = configuration.getConfiguration().getString("theme");
 
         (themeConfiguration = new YamlConfiguration()).load(this.themeFile);
+
+
+
     }
 
     public String get(String path) {
