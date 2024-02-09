d1513d9eec1146bb5f9e001cadea7fb2641175b3
furyzen <furi0xn@gmail.com>
2024-02-09 23:47:34 +0100

    style(dev): removed unused imports

--- plugin/src/main/java/fr/furyzen/oneesan/Oneesan.java
+++ plugin/src/main/java/fr/furyzen/oneesan/Oneesan.java
@@ -1,10 +1,7 @@
 package fr.furyzen.oneesan;
 
 import fr.furyzen.oneesan.registry.CommandRegistry;
-import fr.furyzen.oneesan.utilities.Constants;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 public enum Oneesan {
