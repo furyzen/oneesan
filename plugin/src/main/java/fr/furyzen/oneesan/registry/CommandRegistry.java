36286445dd95a458f39d6deb5a950d3aa32cc3cc
furyzen <furi0xn@gmail.com>
2024-02-14 14:41:30 +0100

    fix(dev): some command system related tweaks and added viaversion to the debug server

--- plugin/src/main/java/fr/furyzen/oneesan/registry/CommandRegistry.java
+++ plugin/src/main/java/fr/furyzen/oneesan/registry/CommandRegistry.java
@@ -4,12 +4,12 @@ import com.mojang.brigadier.CommandDispatcher;
 import fr.furyzen.oneesan.command.AbstractCommand;
 import fr.furyzen.oneesan.command.impl.CommandPing;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.server.dedicated.DedicatedServer;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_20_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -28,23 +28,20 @@ public class CommandRegistry {
     }
 
     public void initialize() {
-        addCommand(CommandPing::new);
+        registerCommand(CommandPing::new);
     }
 
-    public void addCommand(Supplier<AbstractCommand> supplier) {
+    public void registerCommand(Supplier<AbstractCommand> supplier) {
         AbstractCommand command = supplier.get();
 
         commandList.add(command);
         command.register(commandDispatcher);
     }
 
-    public void removeCommand(AbstractCommand command) {
-        commandList.remove(command);
-    }
-
     public Optional<AbstractCommand> getCommandByName(String commandName) {
         return commandList.stream()
-                .filter(command -> command.getCommandName().equals(commandName))
+                .filter(command -> command.getCommandName().equals(commandName)
+                        || Arrays.asList(command.getCommandAliases()).contains(commandName))
                 .findFirst();
     }
 
